0ec9dd3f2fd97e71493adf215b15927abddb8b89
VhiktorBrown <bukkychukwujekwu@gmail.com>
2022-08-15 23:42:56 +0100

    Order History underway

--- lib/pages/food/food_details.dart
+++ lib/pages/food/food_details.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_market/data/models/category.dart';
 import 'package:food_market/utils/app_colors.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -8,7 +9,7 @@ class FoodDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.backgroundColor,
+      backgroundColor: Colors.white,
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
@@ -57,12 +58,12 @@ class FoodDetails extends StatelessWidget {
               child: Container(
                 padding: const EdgeInsets.only(top: 10, right: 10, left: 10, bottom: 10),
                 width: double.maxFinite,
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.only(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(10),
                     topRight: Radius.circular(10)
                   ),
-                  color: AppColors.backgroundColor
+                  color: Colors.white
                 ),
                 child: Text(
                   "Vegan Green Leaf Feast",
@@ -92,16 +93,225 @@ class FoodDetails extends StatelessWidget {
 
           SliverToBoxAdapter(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                ListView.builder(
+                SizedBox(
+                  height: 60,
+                  width: double.maxFinite,
+                  child: ListView.builder(
+                    itemCount: 2,
+                    scrollDirection: Axis.horizontal,
                     itemBuilder: (context, index) {
+                      return Container(
+                        padding: const EdgeInsets.only(left: 10, right: 10),
+                        margin: const EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: AppColors.lightGrey
+                        ),
+                        child: Row(
+                          children: [
+                            Icon(
+                              categories[index].icon,
+                              size: 19,
+                              color: categories[index].color,
+                            ),
+                            const SizedBox(width: 10,),
+                            Text(
+                              categories[index].text,
+                              style: GoogleFonts.nunito(
+                                  fontSize: 16,
+                                  color: AppColors.primaryColor,
+                                  fontWeight: FontWeight.bold
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                  }),
+                ),
 
-                }),
+                const SizedBox(
+                  height: 10,
+                ),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.favorite_border,
+                          color: AppColors.primaryColor,
+                        ),
+                        Text(
+                          "Add to Favorites",
+                          style: GoogleFonts.nunito(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                              color: AppColors.greyColor
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "234",
+                          style: GoogleFonts.nunito(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w800,
+                              color: AppColors.primaryColor
+                          ),
+                        ),
+                        Text(
+                          "Orders",
+                          style: GoogleFonts.nunito(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                              color: AppColors.greyColor
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    //Place holder for Reviews
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "21",
+                          style: GoogleFonts.nunito(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w800,
+                              color: AppColors.primaryColor
+                          ),
+                        ),
+                        Text(
+                          "Reviews",
+                          style: GoogleFonts.nunito(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                              color: AppColors.greyColor
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+
+                const SizedBox(height: 20,),
+
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Text(
+                    "Details",
+                    style: GoogleFonts.nunito(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w800,
+                      color: AppColors.primaryColor
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 10,),
+
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Text(
+                    "This is the details of this particular food. It's the best fit for Vegetarians, enriched with all of nature's finest green stuff. This is the details of this particular food. It's the best fit for Vegetarians, enriched with all of nature's finest green stuff. This is the details of this particular food. It's the best fit for Vegetarians, enriched with all of nature's finest green stuff. This is the details of this particular food. It's the best fit for Vegetarians, enriched with all of nature's finest green stuff. This is the details of this particular food. It's the best fit for Vegetarians, enriched with all of nature's finest green stuff. This is the details of this particular food. It's the best fit for Vegetarians, enriched with all of nature's finest green stuff. This is the details of this particular food. It's the best fit for Vegetarians, enriched with all of nature's finest green stuff. This is the details of this particular food. It's the best fit for Vegetarians, enriched with all of nature's finest green stuff.",
+                    style: GoogleFonts.nunito(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: AppColors.greyColor
+                    ),
+                  ),
+                ),
               ],
             ),
           )
         ],
-      )
+      ),
+
+      bottomNavigationBar: Container(
+        height: 80,
+        color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Row(
+              children: [
+                Container(
+                  width: 100,
+                  height: 40,
+                  padding: const EdgeInsets.only(left: 15, right: 15),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: AppColors.lightGrey
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.remove,
+                        color: AppColors.primaryColor,
+                        size: 16,
+                      ),
+                      Text(
+                        "1",
+                        style: GoogleFonts.nunito(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w900,
+                            decoration: TextDecoration.none,
+                            color: AppColors.primaryColor
+                        ),
+                      ),
+                      Icon(
+                        Icons.add,
+                        color: AppColors.primaryColor,
+                        size: 16,
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(width: 20,),
+
+                Expanded(
+                    child: Container(
+                      height: 50,
+                      padding: const EdgeInsets.only(left: 20, right: 20),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: AppColors.primaryColor,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("Add to cart",
+                          style: GoogleFonts.nunito(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w800,
+                            color: Colors.white
+                          ),
+                          ),
+                          Text("\$34",
+                            style: GoogleFonts.nunito(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w800,
+                                color: Colors.white
+                            ),
+                          ),
+                        ],
+                      ),
+                    ))
+              ],
+            ),
+          ),
+      ),
     );
   }
 }
---
+++ lib/pages/history/order_history.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class OrderHistory extends StatelessWidget {
+  const OrderHistory({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
